67568f25e526d20dc4a1672f4ef32ccea3254deb
Nataliya Sayenko <nataliyasayenko@gmail.com>
2017-02-25 15:13:18 -0500

    add styles

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,7 +6,6 @@ import styles from './styles.scss';
 function App({ children }) {
   return (
     <div>
-      <i className={styles.logo} />
       <ul className={styles.nav}>
         <li className={styles.navItem}>
           <Link className={styles.link} to="/">Home</Link>
