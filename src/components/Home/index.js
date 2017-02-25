6a690a6e9ea1ed529d3c25b16474b648743f92fc
Nataliya Sayenko <nataliyasayenko@gmail.com>
2017-02-25 17:10:01 -0500

    style map some more

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -13,17 +13,27 @@ class Home extends Component {
   svg() {
     const svg = d3.select('svg');
     const path = d3.geoPath();
+    const sampleData ={};
 
     d3.json("https://d3js.org/us-10m.v1.json", (error, us) => {
       if (error) throw error;
-      console.log(svg)
       svg.append("g")
         .attr("class", "states")
           .selectAll("path")
           .data(topojson.feature(us, us.objects.states).features)
           .enter().append("path")
             .attr("class", `${styles.states}`)
-            .attr("d", path);
+            .attr("d", path)
+            .attr('fill', function(d) { 
+              const low = Math.round(100*Math.random());
+              const mid = Math.round(100*Math.random());
+              const high=Math.round(100*Math.random());
+
+              sampleData[d]={low:d3.min([low,mid,high]), high:d3.max([low,mid,high]), 
+                avg:Math.round((low+mid+high)/3), color:d3.interpolate("#7D69EF", "#1A1633")(low/100)}; 
+                return sampleData[d].color;
+            });
+           // .on("click", this.clicked);
 
       svg.append("path")
           .attr("class", `${styles.stateBorders}`)
@@ -31,11 +41,36 @@ class Home extends Component {
     });
   }
 
+  // clicked(d) {
+  //   let x, y, k, centered;
+  //   console.log(d)
+
+  //   if (d && centered !== d) {
+  //     const centroid = path.centroid(d);
+  //     x = centroid[0];
+  //     y = centroid[1];
+  //     k = 4;
+  //     centered = d;
+  //   } else {
+  //     x = width / 2;
+  //     y = height / 2;
+  //     k = 1;
+  //     centered = null;
+  //   }
+
+  //   g.selectAll("path")
+  //       .classed("active", centered && function(d) { return d === centered; });
+
+  //   g.transition()
+  //       .duration(750)
+  //       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+  //       .style("stroke-width", 1.5 / k + "px");
+  // }
+
     render() {
       return (
         <section className={styles.svgHolder}>
-          <svg className={styles.svg}>
-          </svg>
+          <svg className={styles.svg}></svg>
         </section>
       )
     };
