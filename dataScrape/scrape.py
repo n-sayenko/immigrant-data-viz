81a94e04b17337eb6b8edf5dd840cac12fa88746
Nicole <nicole.baram@nytimes.com>
2017-02-25 17:07:43 -0500

    fixed the things + added total pop

--- dataScrape/scrape.py
+++ dataScrape/scrape.py
@@ -1,12 +1,12 @@
 def main():
     # reading and scrubbing a file form NASA
     source_file = "RealData.txt"
-    target_file = "RealData_goodData.txt"
+    target_file = "RealData_goodData.json"
 
     s = open(source_file, 'r')
     t = open(target_file, 'w')
     countRecords, countHeader = 0,0
-    new_text = '{'
+    new_text = '['
     for line in s:
         new_line = ""
         currentLine = line.rstrip("\n")
@@ -16,19 +16,17 @@ def main():
         if (currentLine[:5] != "State"):
             values = currentLine.split()
             new_line += "{"
-            new_line += values[0]+":{"
-            for i in range (1, 5):
-                print("i", i)
-                # new_text += "{"
-                new_line += str(header[i])
-                new_line += ":"
-                new_line += str(values[i])
-                if (i != 4):
+            new_line += "\"" + str(values[0]) + "\": {"
+            for i in range (1, 6):
+                new_line += '\"'+ str(header[i]) + "\""
+                new_line += ": "
+                new_line += "\"" + str(values[i]) + "\""
+                if (i != 5):
                     new_line += ","
-            new_text += "}},\n"
+            new_line += "}},\n"
         new_text += new_line
         countRecords += 1
-    new_text += "}"
+    new_text += "]"
     t.write(new_text)
 
     s.close()
